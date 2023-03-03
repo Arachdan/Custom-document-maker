51845bfa96d54e44f05b77baeb40828da73bfbcc
Daniel Klepacki <daniel@MacBook-Pro-Daniel.local>
2023-03-03 16:49:56 +0100

    Added CV structures

---
+++ Scripts/activateCanvas_&_deactivateCanvas.js
@@ -0,0 +1,7 @@
+function activateCanvas() {
+    canvas.classList.replace('inactive_canvas', 'active_canvas');
+}
+
+function deactivateCanvas() {
+    canvas.classList.replace('active_canvas', 'inactive_canvas');
+}
--- Scripts/showCanvas.js
+++ Scripts/showCanvas.js
@@ -1,14 +1,28 @@
 const document_type_selector = document.getElementsByClassName('select_document_type')[0];
 
 const canvas = document.getElementsByClassName('canvas')[0];
+const ctx = canvas.getContext("2d");
+
+const cv_settings = document.getElementsByClassName('cv_settings')[0];
 
 function showCanvas() {
     switch(document_type_selector.value) {
         case 'cv':
-            canvas.classList.replace('inactive_canvas', 'active_canvas');
+            activateCanvas();
+            activateCVSettings();
 
             break;
         case 'none':
-            canvas.classList.replace('active_canvas', 'inactive_canvas');
+            deactivateCanvas();
+            deactivateCVSettings();
+
+            break;
+        default:
+            document.body.innerText = '';
+            setTimeout(
+                () => alert('Some error occurred.'),
+                100
+            );
+            break;
     }
 }
