57227f7860c0f093ee49d612bce4dca9d2ef95fa
Daniel Klepacki <daniel@MacBook-Pro-Daniel.local>
2023-03-07 21:13:17 +0100

    Added putting education on CV (and functions related to that)

--- Scripts/canvas_obj.js
+++ Scripts/canvas_obj.js
@@ -7,17 +7,67 @@ const canvas_obj = new function() {
 
     this.setFont = (size, style) => {
         this.ctx.font = `${size}px ${style}`;
+        this.font_size = size;
     };
     this.setFont(30, 'Arial');
+
+    this.y = 0;
+    this.y_break = 5;
+    this.setY = (size) => {
+        if(size === 'small') this.y = Math.ceil(this. y + (this.font_size + this.y_break) / 3);
+        else this.y += this.font_size + this.y_break;
+    }
+    this.resetY = () => this.y = 0;
+
+    this.break_sign = '===';
+    this.setBreak = () => {
+        this.setY();
+        this.ctx.fillText(this.break_sign, 10, this.y);
+    }
+
+    this.printHeading = (heading) => {
+        this.setY();
+
+        this.setFont(50, 'Copperplate');
+        
+        this.ctx.fillText(heading, 10, this.y);
+
+        this.setFont(30, 'Arial');
+
+        this.setY('small');
+    }
+    this.printText = (label, text) => {
+        this.setY();
+        this.ctx.fillText(`${label}: ${text}`, 10, this.y);
+    }
     this.loadCVContent = {
         personal_info: () => {
-            this.ctx.fillText(`First name and last name: ${cv_settings_bank.name_and_lastname}`, 10, 50);
-            this.ctx.fillText(`Professional title: ${cv_settings_bank.professional_title}`, 10, 100);
-            this.ctx.fillText(`Phone number: ${cv_settings_bank.phone_nr}`, 10, 150);
-            this.ctx.fillText(`E-mail: ${cv_settings_bank.email}`, 10, 200);
+            this.resetY();
+
+            this.printText('First name and last name', cv_settings_bank.name_and_lastname);
+            this.printText('Professional title', cv_settings_bank.professional_title);
+            this.printText('Phone number', cv_settings_bank.phone_nr);
+            this.printText('E-mail', cv_settings_bank.email);
+
+            this.setBreak();
         },
+
+        education: () => {
+            this.printHeading('Education');
+
+            for(i = 0; i < cv_settings_bank.schools.length; i++) {
+                this.printText('School/University name', cv_settings_bank.schools[i].name);
+                this.printText('City', cv_settings_bank.schools[i].city);
+                this.printText('Start and end date', cv_settings_bank.schools[i].time);
+                this.printText('Additional info', cv_settings_bank.schools[i].additional_info);
+
+                this.setY();
+            }
+        },
+
         all: () => {
             this.loadCVContent.personal_info();
+            this.loadCVContent.education();
         }
     };
 };
--- Scripts/cv_settings_events.js
+++ Scripts/cv_settings_events.js
@@ -1,28 +1,88 @@
-function update_name_and_lastname_on_cv_canvas() {
+function updateNameAndLastnameOnCVCanvas() {
     cv_settings_bank.name_and_lastname = document.getElementById('name_and_lastname_input').value;
     canvas_obj.ctx.clearRect(0, 0, canvas_obj.width, canvas_obj.height);
     canvas_obj.loadCVContent.all();
 }
 
-function update_professional_title_on_cv_canvas() {
+function updateProfessionalTitleOnCVCanvas() {
     cv_settings_bank.professional_title = document.getElementById('professional_title_input').value;
     canvas_obj.ctx.clearRect(0, 0, canvas_obj.width, canvas_obj.height);
     canvas_obj.loadCVContent.all();
 }
 
-function update_phone_nr_on_cv_canvas() {
+function updatePhoneNrOnCVCanvas() {
     cv_settings_bank.phone_nr = document.getElementById('phone_number_input').value;
     canvas_obj.ctx.clearRect(0, 0, canvas_obj.width, canvas_obj.height);
     canvas_obj.loadCVContent.all();
 }
 
-function update_email_on_cv_canvas() {
+function updateEmailOnCVCanvas() {
     cv_settings_bank.email = document.getElementById('email_input').value;
     canvas_obj.ctx.clearRect(0, 0, canvas_obj.width, canvas_obj.height);
     canvas_obj.loadCVContent.all();
 }
 
-document.getElementById('name_and_lastname_input').addEventListener('input', update_name_and_lastname_on_cv_canvas);
-document.getElementById('professional_title_input').addEventListener('input', update_professional_title_on_cv_canvas);
-document.getElementById('phone_number_input').addEventListener('input', update_phone_nr_on_cv_canvas);
-document.getElementById('email_input').addEventListener('input', update_email_on_cv_canvas);
+function addSchool() {
+    function bestowDeletingSchoolFunctionality() {
+        for(i = 0; i < cv_settings_bank.schools.length; i++) {
+            let i_var = i;
+            const deletetion_range = 1;
+            document.getElementsByClassName('delete_school_button')[i].onclick = () => {
+                document.getElementsByClassName('school_list_item')[i_var].remove();
+                cv_settings_bank.schools.splice(i_var, deletetion_range);
+    
+                bestowDeletingSchoolFunctionality();
+            };
+        }
+
+        canvas_obj.ctx.clearRect(0, 0, canvas_obj.width, canvas_obj.height);
+        canvas_obj.loadCVContent.all();
+    }
+
+    let school = new School(
+        document.getElementById('school_name_input').value,
+        document.getElementById('school_city_input').value,
+        document.getElementById('school_start_time_input').value,
+        document.getElementById('school_end_time_input').value,
+        document.getElementById('school_additional_info_input').value
+    );
+
+    cv_settings_bank.schools.push(school);
+
+    canvas_obj.ctx.clearRect(0, 0, canvas_obj.width, canvas_obj.height);
+    canvas_obj.loadCVContent.all();
+
+    document.getElementsByClassName('schools_list')[0].innerHTML = '';
+    for(i = 0; i < cv_settings_bank.schools.length; i++) {
+        document.getElementsByClassName('schools_list')[0].innerHTML += (`
+            <ul class="list-group list-group-horizontal school_list_item mb-1">
+                <li class="list-group-item w-75">
+                    <big>${cv_settings_bank.schools[i].name}</big>
+                </li>
+                <li class="list-group-item w-25 delete_school_button_container">
+                    <button type="button" class="btn-close delete_school_button btn btn-danger" aria-label="Close"></button>
+                </li>
+            </ul>
+        `);
+    }
+
+    bestowDeletingSchoolFunctionality();
+}
+
+document.getElementById('name_and_lastname_input').addEventListener('input', updateNameAndLastnameOnCVCanvas);
+document.getElementById('professional_title_input').addEventListener('input', updateProfessionalTitleOnCVCanvas);
+document.getElementById('phone_number_input').addEventListener('input', updatePhoneNrOnCVCanvas);
+document.getElementById('email_input').addEventListener('input', updateEmailOnCVCanvas);
+
+const education_check_button = document.getElementById('education_check_button');
+const education_check_button_label = document.getElementById('education_check_button_label');
+education_check_button.addEventListener('change', () => {
+    if(education_check_button.checked === true) {
+        education_check_button_label.innerText = 'Active';
+    }
+    else {
+        education_check_button_label.innerText = 'Inactive';
+    }
+});
+
+document.getElementsByClassName('add_school_button')[0].addEventListener('click', addSchool);
